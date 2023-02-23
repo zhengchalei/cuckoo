c428dc02b1c339435d4287adab1b2a26e6b37750
zhengchalei <zhengchalei@outlook.com>
2023-02-24 04:43:04 +0800

    1. 补充 Token 异常范围

--- cuckoo-common/src/main/java/org/github/zhengchalei/common/GlobalException.java
+++ cuckoo-common/src/main/java/org/github/zhengchalei/common/GlobalException.java
@@ -1,8 +1,11 @@
 package org.github.zhengchalei.common;
 
+import cn.dev33.satoken.exception.NotLoginException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
 
 
 @Slf4j
@@ -16,5 +19,27 @@ public class GlobalException {
         return R.fail(e.getMessage());
     }
 
+    @ExceptionHandler(NotLoginException.class)
+    public R<Void> exception(NotLoginException e) {
+        // 如果是 session 存储丢失后也会出现这个问题, 具体还得看 key 的 存储类型
+        if (Objects.equals(e.getType(), NotLoginException.NOT_TOKEN)) {
+            return R.fail("回话已过期, 请重新登录!");
+        }
+        // 正常过期
+        if (Objects.equals(e.getType(), NotLoginException.TOKEN_TIMEOUT)) {
+            return R.fail("回话已过期, 请重新登录!");
+        }
+        // token 异常
+        if (Objects.equals(e.getType(), NotLoginException.NOT_TOKEN)) {
+            return R.fail("未登录, 请登录后使用!");
+        }
+        if (Objects.equals(e.getType(), NotLoginException.BE_REPLACED)) {
+            return R.fail("账号在其他地方登录, 已被迫下线!");
+        }
+        if (Objects.equals(e.getType(), NotLoginException.KICK_OUT)) {
+            return R.fail("被管理员强制下线!");
+        }
+        return R.fail("未登录, 请登录后使用!");
+    }
 
 }
